91e357543a5a290968a3b23b4c95c5e7522aac39
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-26 21:49:19 -0300

    incident list

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import logoImg from "../../assets/logo.png";
 
@@ -19,6 +20,30 @@ export default function Incidents() {
       <Text style={styles.description}>
         Escolha um dos casos abaixo e salve o dia.
       </Text>
+
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.incidentList}
+        keyExtractor={(incident) => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
